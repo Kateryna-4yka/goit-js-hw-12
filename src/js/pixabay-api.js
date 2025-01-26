f594c84012e291d2d2ff76961923253b4051c0aa
Kateryna-4yka <kotasyri@gmail.com>
2025-01-26 23:50:35 +0200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,17 @@
 // У файлі pixabay-api.js зберігай функції для HTTP-запитів.
 
-export function nttpRequest (userRequest) {
-    const paramsForHttp = new URLSearchParams ({
-        q: userRequest,
-        key: `48292121-37734ba9461bf6417934fa015`,
-        image_type: `photo`, 
-        orientation: `horizontal`,
-        safesearch : true,
 
-    })
-    return fetch (`https://pixabay.com/api/?${paramsForHttp}`)
+//імортуємо ахіос
 
-    //обробляємо відповідь що прийшла 
-    .then (queryResult=>{
-    //робимо перевірку, якщо інфа прийшла з помилкою, то перекидаємо у catch
-    if (!queryResult.ok)
-      {throw new Error(queryResult.status);}
-        
-    //а якщо все ок, тоді РОЗПАРШУЄМО данні для наступного then
-    return queryResult.json();
-    })
+import axios from "axios";
 
-
-}
+export function httpRequest (userRequest, page) {
+  return axios.get (`https://pixabay.com/api/?`, {params: {
+    q: userRequest,
+    key: `48292121-37734ba9461bf6417934fa015`,
+    image_type: `photo`, 
+    orientation: `horizontal`,
+    safesearch : true,
+    per_page: 15,
+    page: page,}
+})}
--- src/main.js
+++ src/main.js
@@ -6,64 +6,115 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 //імпортуємо свої функції для НТТР та відмальовування на сторінці лішок
-import {nttpRequest} from "./js/pixabay-api";
+import {httpRequest} from "./js/pixabay-api";
 import {createCart} from "./js/render-functions";
 
-// шукаємо форму і вішаємо на її сабміт слухача 
+
 
 const form = document.querySelector (`.form`);
 const ulGallery = document.querySelector (`.ul-gallery`);
 const button = document.querySelector ('.button');
 const loader = document.querySelector ('.loader');
-
+const loadButton = document.querySelector ('.loadButton');
 form.addEventListener (`submit`, request);
 
-function request (event) {
+let page = 1;
+let valueInfo = '';
+let totalPages = 0; 
+
+async function request (event) {
+try {
+  //лоадер стає видимим
   loader.classList.toggle (`visually-hidden`);
 // подія сабмін робить оновлення сторінки, ми це скидаємо 
 event.preventDefault();
 
 //можна звернутися до input.value , але це зайвий раз звертатися до дерева , тому ідемо через event.currentTarget.elements.(ім,я з name HTML) + .trim(), щоб не було пробілів
+//считуємо значення з інпуту
+valueInfo = event.currentTarget.elements.user_request.value.trim(); 
 
-const userRequest = event.currentTarget.elements.user_request.value.trim(); 
+//перевіряємо, щоб не вводили пусте в інпут + HYML перевірка задана
+if (valueInfo ==='') {button.disabled = true; return;}
 
-//перевіряємо, щоб не вводили пусте в інпут
-if (userRequest ==='') {button.disabled = true; return;}
-
+//робимо запит за пайдж 1, щоб кожен новий запит ішов за першою сторінкою та галерея очищувалася від картинок
+page = 1;
+ulGallery.innerHTML = '';
 //робимо запит на сервер pixabay.com за своїм клієнт ключем та параметрами з документації АРІ
-nttpRequest (userRequest)
-.then (serverInform => {
+const serverInform = await httpRequest (valueInfo, page);
 
-//перевірка справжніх слів
-if (serverInform.hits.length === 0) {
+//перевірка результатів з сервера, чи прийшли данні
+if (serverInform.data.hits.length === 0) {
   loader.classList.toggle (`visually-hidden`);
   iziToast.error({
     position: 'topRight' ,
     message: 'Sorry, there are no images matching your search query. Please try again!',
 });
-
-
 ulGallery.innerHTML = '';
 form.reset();
 return;}
 
- const gallaryCard = serverInform.hits.map(el =>createCart(el)).join('');
+//вираховуємо кількість сторінок для відмальовування по нашому запиту
+totalPages = Math.ceil(serverInform.data.totalHits/15); 
+
+//якщо інформація що прийшла з сервера більша за 1 сторінку (тобто, 15 картинок), тоді кнопка видима, слухаємо клік 
+if (totalPages>1) {loadButton.classList.remove (`visually-hidden`);
+  loadButton.addEventListener ('click', loadMore);}
+
+const gallaryCard = serverInform.data.hits.map(el =>createCart(el)).join('');
 // метод меп перебирає масив об,єктів і видає на кожній ітерації 1 обєкт , який заповнений рядками з описами кожної картки 
 //але нам потрібен 1 рядок, а не масив рядків, тому ми викликаємо метод .join  і зшиваємо це до купи
-
 //робимо функцію createCart, яка буде перетворювати отримані данні з сервера на розмітку на сторінці. Створювати картку в HTML з данними, які прийшли з сервера. 
 
-
-//додаємо на сторінку через innerHTML у знайдений список
-ulGallery.innerHTML = gallaryCard;
+//відмальовуємо на сторінці
+ulGallery.insertAdjacentHTML('beforeend', gallaryCard);
+//відмальовування почалося і лоадер сховався
 loader.classList.toggle (`visually-hidden`);
+//спустошуємо інпут у формі
 form.reset();
+
 //відмальовуємо великі зображення за допомогою бібліотеки SimpleLightbox
 const elemSimpleLightbox = new SimpleLightbox(`.ul-gallery a`, {
   captionsData: `alt`, 
 });
 elemSimpleLightbox.refresh();
-})
+}
+ catch (er) { alert (er)}}
 
-.catch (error=>{console.log(error);})};
 
+
+//слухач викликає функцію запиту на сервер за page++
+ async function loadMore (event){
+  page++;
+  try {
+    //робимо видимим лоадер при завантаженні інформації
+    loader.classList.toggle (`visually-hidden`);
+
+  //робимо запит на сервер за наступною сторінкою інформації
+  const serverInform = await httpRequest (valueInfo, page);
+
+
+//якщо інформація поділена на кожну сторінку вже дорівнює кількості сторінок , то вимикаємо кнопку ЛОАД МОР
+if (totalPages === page)
+  {loadButton.classList.add (`visually-hidden`); 
+    loadButton.removeEventListener('click', loadMore);
+    iziToast.info({
+      message: "We're sorry, but you've reached the end of search results.",
+      position: 'topRight' ,
+  });}
+
+  const gallaryCard = serverInform.data.hits.map(el =>createCart(el)).join('');
+//перебираємо масив інформації та видаємо її у вигляді рядка для запису на сторінку 
+  
+  //додаємо на сторінку
+  ulGallery.insertAdjacentHTML('beforeend', gallaryCard);
+  //ховаємо лоадер після відмальовування
+  loader.classList.toggle (`visually-hidden`);
+  
+  //відмальовуємо великі зображення за допомогою бібліотеки SimpleLightbox
+  const elemSimpleLightbox = new SimpleLightbox(`.ul-gallery a`, {
+    captionsData: `alt`, 
+  });
+  elemSimpleLightbox.refresh();
+  }
+  
+   catch (er) {console.log(er);}}
